ee2ae5daa7642b62ffa82391aa2aecb6ee36324f
shivammmmm <shivampa345@gmail.com>
2026-01-02 19:24:57 +0530

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -262,49 +262,49 @@ export const PROJECTS = [
     title: "PetroShop E-commerce Website",
     description:
       'Discover the future of online shopping with "PetroShop E-commerce Website" - a cutting-edge e-commerce platform designed to revolutionize your shopping experience. Seamlessly blending technology and convenience, PetroShop offers a user-friendly interface, secure transactions, and a vast array of products at your fingertips. Whether you\'re a tech enthusiast or a casual shopper, PetroShop is your gateway to a world of endless possibilities.',
-    image: "/projects/project-1.png",
+    image: "projects/project-1.png",
     link: "https://thepetroshop.com/",
   },
   {
     title: "MyIndustryHouse B2B Marketplace",
     description:
       'Step into the world of industrial commerce with "MyIndustryHouse B2B Marketplace" - a dynamic platform tailored for businesses seeking seamless connections and efficient transactions. MyIndustryHouse serves as a hub for manufacturers, suppliers, and buyers to collaborate, negotiate, and thrive in the competitive B2B landscape. Experience a new era of industrial trade where opportunities abound and partnerships flourish.',
-    image: "/projects/project-2.png",
+    image: "projects/project-2.png",
     link: "https://myindustryhouse.com/",
   },
   {
     title: "Standard Petro India Pvt Ltd Website",
     description:
       'Explore the digital presence of "Standard Petro India Pvt Ltd" - a leading name in the petroleum industry. This website serves as a comprehensive platform showcasing the company\'s commitment to excellence, innovation, and sustainability. From detailed insights into their services to the latest industry news, the Standard Petro India Pvt Ltd website is your go-to destination for all things related to petroleum solutions.',
-    image: "/projects/project-3.png",
+    image: "projects/project-3.png",
     link: "https://standardpetro.in/",
   },
   {
     title: "Coffee Cafe Website",
     description:
       'Indulge in the aromatic world of "Coffee Cafe Website" - a virtual haven for coffee enthusiasts and connoisseurs alike. This website is designed to capture the essence of a cozy cafe experience, offering a delightful array of coffee blends, brewing techniques, and café culture. Whether you\'re seeking brewing tips or simply want to explore the rich history of coffee, the Coffee Cafe Website is your ultimate guide to all things coffee.',
-    image: "/projects/project-4.png",
+    image: "projects/project-4.png",
     link: "https://rohit-rich.github.io/coffee/",
   },
   {
     title: "Radio World Wide Website",
     description:
       'Tune into the vibrant world of "Radio World Wide Website" - your gateway to a global radio experience. This website is a dynamic platform that brings together diverse radio stations from around the world, offering a rich tapestry of music, talk shows, and cultural programming. Whether you\'re a music lover or a curious listener, Radio World Wide Website provides an immersive audio journey that transcends borders and connects communities through the power of radio.',
-    image: "/projects/project-5.png",
+    image: "projects/project-5.png",
     link: "https://radioworldwide.in/",
   },
   {
     title: "Radio Corporation Website",
     description:
       'Tune into the vibrant world of "Radio World Wide Website" - your gateway to a global radio experience. This website is a dynamic platform that brings together diverse radio stations from around the world, offering a rich tapestry of music, talk shows, and cultural programming. Whether you\'re a music lover or a curious listener, Radio World Wide Website provides an immersive audio journey that transcends borders and connects communities through the power of radio.',
-    image: "/projects/project-6.png",
+    image: "projects/project-6.png",
     link: "https://iradiocorporation.com/",
   },
   {
     title: "Portfolio Website",
     description:
       'A personal portfolio website to showcase my projects, skills, and experience as a developer. It features a clean and modern design, responsive layout, and smooth animations to provide an engaging user experience. The website is built using Next.js and Tailwind CSS, ensuring optimal performance and scalability.',
-    image: "/projects/project-7.png",
+    image: "projects/project-7.png",
     link: "https://sujalrathore.online/",
   }
 ] as const;

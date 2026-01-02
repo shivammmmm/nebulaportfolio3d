36f88d6187912a3b0ff48879ab8fe27322d442b7
shivammmmm <shivampa345@gmail.com>
2026-01-02 19:16:40 +0530

    Update encryption.tsx

--- src/components/main/encryption.tsx
+++ src/components/main/encryption.tsx
@@ -24,14 +24,14 @@ export const Encryption = () => {
       <div className="flex flex-col items-center justify-center translate-y-[-50px] absolute z-[20] w-auto h-auto">
         <div className="flex flex-col items-center group cursor-pointer w-auto h-auto">
           <Image
-            src="/lock-top.png"
+            src="lock-top.png"
             alt="Lock top"
             width={50}
             height={50}
             className="translate-y-5 transition-all duration-200 group-hover:translate-y-11"
           />
           <Image
-            src="/lock-main.png"
+            src="lock-main.png"
             alt="Lock main"
             width={70}
             height={70}

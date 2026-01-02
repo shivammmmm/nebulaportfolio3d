244dd2fb860f3a93d6543caec0c5797263c7a047
shivammmmm <shivampa345@gmail.com>
2026-01-02 19:22:01 +0530

    Update encryption.tsx

--- src/components/main/encryption.tsx
+++ src/components/main/encryption.tsx
@@ -59,7 +59,7 @@ export const Encryption = () => {
           preload="false"
           className="w-full h-auto"
         >
-          <source src="/videos/encryption-bg.webm" type="video/webm" />
+          <source src="videos/encryption-bg.webm" type="video/webm" />
         </video>
       </div>
     </div>
